fdfcf1475a53fb4aa19c16e7a3cbce5f7658d035
Judy McGoldrick <jjeanius@verizon.net>
2018-01-02 00:56:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,10 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand, :brands
-
-  class BRANDS
+  
   BRANDS = []
 
-
-
+  class BRANDS
 
   def initialize(brand)
     @brand = brand
